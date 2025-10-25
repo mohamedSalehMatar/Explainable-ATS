3b3ec843ae0c8720ba0856ba79823f9c717c4aca
mohamedSalehMatar <mohamedsalehmatar@gmail.com>
2025-10-25 11:13:28 +0300

    adding train-test split and display sets

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Intialize dataset path
 path='data_raw/generated_data.csv'
@@ -7,4 +8,26 @@ path='data_raw/generated_data.csv'
 raw_df = pd.read_csv(path)
 
 # Display the DataFrame
-print(raw_df.to_string()) 
+print(raw_df.head()) 
+
+# using the train test split function
+X= raw_df[['resume_text', 'job_text']]
+y= raw_df['fit_score']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                   random_state=104, 
+                                   test_size=0.25, 
+                                   shuffle=True)
+
+# printing out train and test sets
+print('X_train : ')
+print(X_train.head())
+print('')
+print('X_test : ')
+print(X_test.head())
+print('')
+print('y_train : ')
+print(y_train.head())
+print('')
+print('y_test : ')
+print(y_test.head())

ea32f3ff64ecce3be8e7c3c2c47a64f25c856c74
mohamedSalehMatar <mohamedsalehmatar@gmail.com>
2025-11-01 09:31:06 +0200

    Update dataset loader to download from new Kaggle dataset and extract text from PDF

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -3,12 +3,24 @@ from sklearn.model_selection import train_test_split
 
 import kagglehub
 
-# Intialize dataset 
-path = kagglehub.dataset_download("gauravduttakiit/resume-dataset") + "/UpdatedResumeDataSet.csv"
+import os
+from pypdf import PdfReader
+
+# Downloading dataset from Kaggle
+path = kagglehub.dataset_download("snehaanbhawal/resume-dataset")
 print("Path to dataset files:", path)
+it_resumes_path = path + r"\data\data\INFORMATION-TECHNOLOGY"
+print("Path to IT resumes dataset files:", it_resumes_path)
 
-# Load dataset into a DataFrame
-raw_df = pd.read_csv(path)
+# creating a pdf reader object
+reader = PdfReader(os.path.join(it_resumes_path, os.listdir(it_resumes_path)[0]))
 
-# Display the DataFrame
-print(raw_df.head())
+# printing number of pages in pdf file
+print(len(reader.pages))
+
+# getting a specific page from the pdf file
+page = reader.pages[0]
+
+# extracting text from page
+text = page.extract_text()
+print(text)
